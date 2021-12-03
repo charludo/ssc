0c791de5f3b7a69a80ac0cbe3c94a6c0d455f99a
charludo <github@charlotteharludo.com>
2021-12-03 10:54:22 +0100

    made progress on addition!

--- compiler.py
+++ compiler.py
@@ -1,4 +1,25 @@
 from lark import Tree
+from itertools import product
+
+
+def prep_args(func):
+    def prep_value(value):
+        if value is None:
+            return "num", 0
+        elif isinstance(value, int):
+            return "num", value
+        else:
+            return value, [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    def inner(left, right):
+        name, value = prep_value(left)
+        le = dict(name=name, value=value)
+
+        name, value = prep_value(right)
+        ri = dict(name=name, value=value)
+
+        return func(le, ri)
+    return inner
 
 
 class Compiler:
@@ -38,11 +59,26 @@ class Compiler:
 
     @staticmethod
     def EQ(field, value_map):
-        return f"{field} = {value_map}"
+        print(value_map)
+        const, names, combs = value_map
+        if const:
+            return f"{field}{const}"
 
     @staticmethod
+    @prep_args
     def ADD(left, right):
-        return f"{left} + {right}"
+        if left["name"] == "num" and right["name"] == "num":
+            return left["value"] + right["value"], [], []
+        elif left["name"] == "num":
+            return None, [right["name"]], [[(i-left["value"],)] if i-left["value"] > 0 else [] for i in right["value"]]
+        elif right["name"] == "num":
+            return None, [left["name"]], [[(i-right["value"],)] if i-right["value"] > 0 else [] for i in left["value"]]
+        else:
+            const = None
+            names = [left["name"], right["name"]]
+            cartesian = list(product(left["value"], right["value"]))
+            cartesian = [[c for c in cartesian if c[0] + c[1] == i] for i in range(1, 10)]
+            return const, names, cartesian
 
     comparison_map = {
         "=": EQ.__func__
