8b52f6a929109cc1d8196ffb452a396a02db2b17
charludo <github@charlotteharludo.com>
2022-02-06 16:52:19 +0100

    published to pypi

---
+++ README.md
@@ -0,0 +1,27 @@
+# Sudoku-SAT-compiler (ssc)
+
+Describe generalized Sudokus through simple equations. Compile programs to SAT and solve them with `limboole`.
+
+See `examples/` for example programs.
+
+See `ssc --help` for all options.
+
+Required Python version: `>= 3.9`
+
+## Recommended Installation Instructions
+
+- install `limboole`, either through your system's package manager or manually
+- install `ssc`:
+	- `python -m venv .ssc`
+	- `. ./ssc/bin/activate`
+	- `pip install ssc-compiler`
+
+## Manual Installation Instructions
+
+- install `limboole`, either through your system's package manager or manually
+	- install `ssc`:
+	- `git clone git@github.com:charludo/ssc.git`
+	- `python -m venv .ssc`
+	- `. .ssc/bin/activate`
+	- `cd ssc`
+	- `pip install -e .`
--- setup.py
+++ setup.py
@@ -1,8 +1,26 @@
+import pathlib
 import setuptools
 
-setuptools.setup(name='ssc',
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setuptools.setup(name='ssc-compiler',
+                 version="1.0.0",
+
+                 description='Describe generalized Sudokus through simple equations. Compile programs to SAT and solve them with limboole.',
+                 long_description=README,
+                 long_description_content_type="text/markdown",
+                 keywords=['sudoku', 'sat', 'compiler', 'limboole'],
+                 url='https://github.com/charludo/ssc',
+                 author='Charlotte Hartmann Paludo',
+                 author_email='charlotte.hartmann-paludo@stud.uni-due.de',
                  packages=setuptools.find_packages(),
                  include_package_data=True,
+                 zip_safe=False,
+
                  entry_points={"console_scripts": ["ssc = src.ssc:run"]},
                  install_requires=[
                     "lark>=1.0.0",
